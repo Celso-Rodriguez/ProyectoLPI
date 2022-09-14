438adbfc0a941ac17282eca76ae1c96b8b1b82ce
Celso-Rodriguez <celso.19rod@gmail.com>
2022-09-13 20:20:54 -0500

    Agregacion de Titulo (RegistroAlumno)

--- src/gui/FrmRegistroAlumno.java
+++ src/gui/FrmRegistroAlumno.java
@@ -2,6 +2,9 @@ package gui;
 
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class FrmRegistroAlumno extends JInternalFrame {
 
@@ -15,5 +18,11 @@ public class FrmRegistroAlumno extends JInternalFrame {
 		setTitle("Ingreso de Alumno");
 		setBounds(100, 100, 900, 600);
 		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Registrar Alumno");
+		lblNewLabel.setFont(new Font("Arial Narrow", Font.ITALIC, 32));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 11, 864, 61);
+		getContentPane().add(lblNewLabel);
 	}
 }
