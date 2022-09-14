5616bd2536ae1c0bc45c62058273415b6ec54564
naive00 <bjflores0000@gmail.com>
2022-09-13 20:20:36 -0500

    Cambios en formulario tesis

--- src/gui/FrmRegistroTesis.java
+++ src/gui/FrmRegistroTesis.java
@@ -2,12 +2,16 @@ package gui;
 
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class FrmRegistroTesis extends JInternalFrame {
 
 	private static final long serialVersionUID = 1L;
 
 	public FrmRegistroTesis() {
+		getContentPane().setFont(new Font("Tw Cen MT Condensed", Font.PLAIN, 13));
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setMaximizable(true);
 		setIconifiable(true);
@@ -15,5 +19,11 @@ public class FrmRegistroTesis extends JInternalFrame {
 		setTitle("Ingreso de Tesis");
 		setBounds(100, 100, 900, 600);
 		getContentPane().setLayout(null);
+		
+		JLabel lblTituloTesis = new JLabel("Registro de Tesis");
+		lblTituloTesis.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		lblTituloTesis.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTituloTesis.setBounds(10, 21, 864, 27);
+		getContentPane().add(lblTituloTesis);
 	}
 }
