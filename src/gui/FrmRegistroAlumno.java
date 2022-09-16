0a3d135a2c005d8b532cac1944a6a2314387caad
Celso-Rodriguez <celso.19rod@gmail.com>
2022-09-15 23:24:41 -0500

    Termino de Registro Alumno

--- src/gui/FrmRegistroAlumno.java
+++ src/gui/FrmRegistroAlumno.java
@@ -1,23 +1,28 @@
 package gui;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
 import java.util.ResourceBundle;
 
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import entidad.Alumno;
+import entidad.Pais;
+import model.AlumnoModel;
 import util.JComboBoxBD;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import util.Validaciones;
 
 public class FrmRegistroAlumno extends JInternalFrame implements ActionListener {
+	
+	private ResourceBundle rb = ResourceBundle.getBundle("combo");
 
 	private static final long serialVersionUID = 1L;
 	private JTextField txtNombre;
@@ -25,10 +30,12 @@ public class FrmRegistroAlumno extends JInternalFrame implements ActionListener 
 	private JTextField txtTelefono;
 	private JTextField txtCorreo;
 	private JTextField txtFechaDeNacimiento;
-	private JButton btnAgregar;
-	private JComboBox cboPais;
+	private JButton btnAgregar, btnEliminar;
+	private JComboBoxBD cboPais;
+	private JTextField txtDni;
 	
-	private ResourceBundle rb = ResourceBundle.getBundle("combo");
+	
+
 
 	public FrmRegistroAlumno() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -48,79 +55,144 @@ public class FrmRegistroAlumno extends JInternalFrame implements ActionListener 
 
 		JLabel lblNombre = new JLabel("Nombre :");
 		lblNombre.setFont(new Font("Arial Narrow", Font.BOLD | Font.ITALIC, 18));
-		lblNombre.setBounds(150, 119, 103, 22);
+		lblNombre.setBounds(154, 119, 82, 22);
 		getContentPane().add(lblNombre);
 
 		JLabel lblApellido = new JLabel("Apellido :");
 		lblApellido.setFont(new Font("Arial Narrow", Font.BOLD | Font.ITALIC, 18));
-		lblApellido.setBounds(150, 170, 76, 22);
+		lblApellido.setBounds(567, 119, 82, 22);
 		getContentPane().add(lblApellido);
 
 		JLabel lblTelefono = new JLabel("Tel\u00E9fono :");
 		lblTelefono.setFont(new Font("Arial Narrow", Font.BOLD | Font.ITALIC, 18));
-		lblTelefono.setBounds(150, 216, 82, 22);
+		lblTelefono.setBounds(148, 216, 82, 22);
 		getContentPane().add(lblTelefono);
 
 		JLabel lblCorreo = new JLabel("Correo :");
 		lblCorreo.setFont(new Font("Arial Narrow", Font.BOLD | Font.ITALIC, 18));
-		lblCorreo.setBounds(157, 262, 69, 22);
+		lblCorreo.setBounds(576, 168, 69, 22);
 		getContentPane().add(lblCorreo);
 
 		JLabel lblFechaDeNacimiento = new JLabel("Fecha de Nacimiento :");
 		lblFechaDeNacimiento.setFont(new Font("Arial Narrow", Font.BOLD | Font.ITALIC, 18));
-		lblFechaDeNacimiento.setBounds(480, 168, 169, 22);
+		lblFechaDeNacimiento.setBounds(61, 264, 169, 22);
 		getContentPane().add(lblFechaDeNacimiento);
 
 		txtNombre = new JTextField();
-		txtNombre.setBounds(267, 123, 153, 20);
+		txtNombre.setBounds(250, 123, 174, 20);
 		getContentPane().add(txtNombre);
 		txtNombre.setColumns(10);
 
 		txtApellido = new JTextField();
 		txtApellido.setColumns(10);
-		txtApellido.setBounds(267, 174, 153, 20);
+		txtApellido.setBounds(655, 123, 184, 20);
 		getContentPane().add(txtApellido);
 
 		txtTelefono = new JTextField();
 		txtTelefono.setColumns(10);
-		txtTelefono.setBounds(267, 220, 153, 20);
+		txtTelefono.setBounds(244, 220, 180, 20);
 		getContentPane().add(txtTelefono);
 
 		txtCorreo = new JTextField();
 		txtCorreo.setColumns(10);
-		txtCorreo.setBounds(267, 266, 153, 20);
+		txtCorreo.setBounds(659, 172, 180, 20);
 		getContentPane().add(txtCorreo);
 
 		txtFechaDeNacimiento = new JTextField();
 		txtFechaDeNacimiento.setColumns(10);
-		txtFechaDeNacimiento.setBounds(659, 172, 184, 20);
+		txtFechaDeNacimiento.setBounds(240, 268, 184, 20);
 		getContentPane().add(txtFechaDeNacimiento);
 
 		btnAgregar = new JButton("AGREGAR");
 		btnAgregar.addActionListener(this);
-		btnAgregar.setBounds(283, 329, 169, 51);
+		btnAgregar.setBounds(286, 339, 169, 51);
 		getContentPane().add(btnAgregar);
 
 		JLabel lblPais = new JLabel("Pa\u00EDs :");
 		lblPais.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblPais.setFont(new Font("Arial Narrow", Font.BOLD | Font.ITALIC, 18));
-		lblPais.setBounds(490, 216, 103, 22);
+		lblPais.setBounds(505, 248, 103, 22);
 		getContentPane().add(lblPais);
 
 		cboPais = new JComboBoxBD(rb.getString("SQL_PAIS"));
-		cboPais.setBounds(603, 219, 240, 22);
+		cboPais.setBounds(618, 251, 240, 22);
 		getContentPane().add(cboPais);
 		
-		JButton btnEliminar = new JButton("ELIMINAR");
-		btnEliminar.setBounds(502, 329, 169, 51);
+		btnEliminar = new JButton("ELIMINAR");
+		btnEliminar.setBounds(560, 339, 169, 51);
 		getContentPane().add(btnEliminar);
+		
+		JLabel lblDni = new JLabel("N\u00B0 DNI :");
+		lblDni.setFont(new Font("Arial Narrow", Font.BOLD | Font.ITALIC, 18));
+		lblDni.setBounds(161, 168, 69, 22);
+		getContentPane().add(lblDni);
+		
+		txtDni = new JTextField();
+		txtDni.setColumns(10);
+		txtDni.setBounds(244, 172, 180, 20);
+		getContentPane().add(txtDni);
 	}
+	
+	
+	public void mensaje(String ms){
+		JOptionPane.showMessageDialog(this, ms);
+	}
+	
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == btnAgregar) {
 			actionPerformedBtnAgregarJButton(e);
 		}
 	}
 	protected void actionPerformedBtnAgregarJButton(ActionEvent e) {
+		String nom = txtNombre.getText().trim();
+		String ape = txtApellido.getText().trim();
+		String tel = txtTelefono.getText().trim();
+		String dni = txtDni.getText().trim();
+		String cor = txtCorreo.getText().trim();
+		String fec = txtFechaDeNacimiento.getText().trim();
+		int index = cboPais.getSelectedIndex();
+		
+		if(!nom.matches(Validaciones.TEXTO)) {
+			mensaje ("El NOMBRE es de 2 a 20 caracteres");
+		}else if (!ape.matches(Validaciones.TEXTO)) {
+			mensaje ("El APELLIDO es de 2 a 20 caracteres");
+		}else if(!tel.matches(Validaciones.FONO)) {
+			mensaje ("El TELEFONO debe empezar con 9 ");
+		}else if(!dni.matches(Validaciones.DNI)) {
+			mensaje ("El DNI es de 8 digitos");
+		}else if(!cor.matches(Validaciones.CORREO)) {
+			mensaje ("El CORREO debe tener un '@' y '.'");
+		}else if (!fec.matches(Validaciones.FECHA)) {
+			mensaje("La fecha tiene formato YYYY-MM-dd");
+		}else if(index==0) {
+			mensaje("Seleccione un Pais");
+		}else {
+			String pais = cboPais.getSelectedItem().toString();
+			String idPais = pais.split(":")[0];
+			
+			Pais objPais = new Pais();
+			objPais.setIdPais(Integer.parseInt(idPais));
+			
+			Alumno objAlumno = new Alumno();
+			objAlumno.setNombres(nom);
+			objAlumno.setApellidos(ape);
+			objAlumno.setTelefono(tel);
+			objAlumno.setDni(dni);
+			objAlumno.setCorreo(cor);
+			objAlumno.setFechaNacimiento(Date.valueOf(fec));
+			objAlumno.setEstado(1);
+			objAlumno.setPais(objPais);
+			
+			AlumnoModel model = new AlumnoModel();
+			int s = model.insertaAlumno(objAlumno);
+			
+			if (s > 0) {
+				mensaje("SE INSERTO CORRECTAMENTE");
+			}else {
+				mensaje("ERROR EN EL REGISTRO");
+			}
+			
+		}
 		
 		
 	}
