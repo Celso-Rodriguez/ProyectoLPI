440122f19a76cb636e75de45f1c9923ac64a2e25
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-09-15 21:08:06 -0500

    Ajustes

--- src/gui/FrmRegistroLibro.java
+++ src/gui/FrmRegistroLibro.java
@@ -1,21 +1,19 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import util.JComboBoxBD;
-
-import java.awt.Font;
-import java.util.ResourceBundle;
-
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import java.awt.Color;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class FrmRegistroLibro extends JInternalFrame implements ActionListener {
 
