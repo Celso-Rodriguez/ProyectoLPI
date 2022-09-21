a04e0cd8a80b5ab1081461e7abd0096d29ef5fcc
JhonatanCarlosChavez <i202122464@cibertec.edu.pe>
2022-09-20 23:35:35 -0500

    CL1 Terminado

---
+++ src/entidad/Proveedor.java
@@ -0,0 +1,82 @@
+package entidad;
+
+import java.sql.Date;
+
+public class Proveedor {
+	
+	public int idProveedor;
+	public String nombres;
+	public String apellidos;
+	public String dni;
+	public String direccion;
+	public String telefono;
+	public String correo;
+	public Date fechaRegistro;
+	private int estado;
+	private Pais pais;
+	
+	
+	public int getIdProveedor() {
+		return idProveedor;
+	}
+	public void setIdProveedor(int idProveedor) {
+		this.idProveedor = idProveedor;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+	
+	
+}
