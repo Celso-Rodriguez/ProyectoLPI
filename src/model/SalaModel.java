6415590a5323b26b740283a7ad0da97b0ee98f3a
Dunshiro <hector_gd_157@hotmail.com>
2022-09-26 05:09:11 -0500

    Modelo Sala

---
+++ src/model/SalaModel.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.logging.Logger;
+
+import entidad.Sala;
+import util.MySqlDBConexion;
+
+public class SalaModel {
+	
+	private static Logger log = Logger.getLogger(SalaModel.class.getName());
+
+	public int insertaSala(Sala obj) {
+		int salida = -1;
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			conn = MySqlDBConexion.getConexion();
+			
+			String sql = "insert into sala value(null,null,?,?,?,curtime(),1,?);";
+			pstm = conn.prepareStatement(sql);
+			pstm.setInt(1, obj.getPiso());
+			pstm.setInt(2, obj.getNumAlumnos());
+			pstm.setString(3, obj.getRecursos());
+			pstm.setInt(5, obj.getSede().getIdSede());		
+			log.info(">>> " + pstm);
+			
+			salida = pstm.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			try {
+				if (pstm != null)
+					pstm.close();
+				if (conn != null)
+					conn.close();
+
+			} catch (Exception e2) {
+			}
+
+		}
+
+		return salida;
+	}
+
+}
