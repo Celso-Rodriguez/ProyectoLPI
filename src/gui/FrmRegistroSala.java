d47208e89ed9432b6d90fa381f428598602aef17
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-09-27 19:14:34 -0500

    Ajustes

--- src/gui/FrmRegistroSala.java
+++ src/gui/FrmRegistroSala.java
@@ -1,9 +1,16 @@
 package gui;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import entidad.Sala;
@@ -11,15 +18,6 @@ import entidad.Sede;
 import model.SalaModel;
 import util.JComboBoxBD;
 import util.Validaciones;
-
-import java.awt.Font;
-import java.util.ResourceBundle;
-
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class FrmRegistroSala extends JInternalFrame implements ActionListener {
 
